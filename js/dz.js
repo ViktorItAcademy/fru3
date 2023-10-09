43de80cc8ffd515492032399179ac4c5cf8bf017
ViktorItAcademy <144340362+ViktorItAcademy@users.noreply.github.com>
2023-10-09 20:12:19 +0500

    Upload files

---
+++ js/dz.js
@@ -0,0 +1,26 @@
+let num1 = 3
+let num2 = 5
+console.log(num1, num2)
+console.log(num2)
+// num1 = num1 + 5
+num1 += 5
+// num2 = num2 * 10
+num2 *= 10
+console.log(num1, num2)
+
+let str1 = 'Привет', str2 = "5"
+console.log(str1, str2)
+
+let strNum1 = 5, strNum2 = "10", strNum3 = 10
+console.log(strNum1 + strNum3 + strNum2)
+console.log(strNum1 - strNum2)
+console.log(strNum1 * strNum2)
+console.log(strNum1 / strNum2)
+console.log(strNum1 ** strNum2)
+
+
+let num5 = 3, num6 = 5
+num5 = num5 + num6 //8
+num6 = num5 - num6 //8 - 5 = 3
+num5 = num5 - num6 //8 - 3 = 5
+console.log(num5, num6)
